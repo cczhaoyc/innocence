c8a253e93f36e6c6cdea89e59cc03bbf7899c630
cczhaoyc <cczhaoyc@163.com>
2019-01-07 17:19:57 +0800

    innocence project for Spring Boot

---
+++ src/main/java/com/suxia/innocence/InnocenceApplication.java
@@ -0,0 +1,14 @@
+package com.suxia.innocence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnocenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnocenceApplication.class, args);
+    }
+
+}
+
